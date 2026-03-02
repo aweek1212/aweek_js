67cf85430298260e1d38a2eb81c4e5471e4a4351
aweek1212 <aweek251212@gmail.com>
2026-03-02 23:56:31 +0900

    js prac1

--- js_prac.js
+++ js_prac.js
@@ -1,2 +1,12 @@
-const name = prompt("이름을 입력하세요.", "이름");
-//console.log(name);
+// 우선순위
+// 남자이고, 이름이 Mike 이거나 성인이면 통과
+
+const gender = 'F';
+const name = 'Jane';
+const isAdult - true;
+
+if(gender === 'M' && name === 'Mike' || isadult){
+    console.log('통과')
+} else {
+    console.log('돌아가.')
+}
